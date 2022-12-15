afeac328101a6edf12af4de42f2993d8e2a84d58
Marcia Alvarez <alvarezmarciabelen@gmail.com>
2022-12-15 20:24:47 -0300

    Endpoints y pruebas - Login y usuario invitado

--- app.py
+++ app.py
@@ -5,36 +5,70 @@ from flask import Flask, jsonify, request
 from users import users
 from movies import movies
 
-
-
 app = Flask(__name__)
 
+#Numero de ID para peliculas
+id = 0
+
+#Inicio del sitio (modulo publico)
+@app.route('/', methods=['GET'])
+def index():
+    #10 últimas peliculas cargadas
+    latest_movies = []
+    
+    if id <= 0:
+        return "No hay peliculas cargadas en la plataforma aún"
+
+    elif id < 10:
+        for movie in range(1, len(movies)+1):
+            movie_dictionary = next((element for element in movies if element["ID"] == (movie)), None)
+            latest_movies.append(movie_dictionary["Titulo"]) 
+        
+        return (latest_movies)
+    else: 
+        for movie in movies(id-10, id):
+            movie_dictionary = next((element for element in movies if element["ID"] == movie), None)
+            latest_movies.append(movie_dictionary["Titulo"]) 
+            
+        return (latest_movies)
+
+print (index())
+
+
 #Autentificación usuario - Login user
-@app.route('/ingresar_usuario', methods=['CONNECT']) 
-def login_user():
+@app.route('/login/<user>', methods=['GET']) 
+def login_user(user, password):
+
 
 
 #Agregar una reseña - Add a review
 @app.route('/agregar_reseña', methods=['POST'])
 def add_review():
 
+
+#ABM de cada pelicula
 #Agregar una pelicula - Add a movie
 @app.route('/agregar_pelicula', methods=["POST"])
 def add_movie():
+    #id += 1
+
     new_movie = {
         "Titulo": request.json['Titulo'], 
         "Anio": request.json ['Anio'], 
         "Director": request.json ['Director'], #En vez de pedirlo tendriamos que obtenerlo de la lista
         "Genero":  request.json ['Genero'], #En vez de pedirlo tendriamos que obtenerlo de la lista
         "Sinopsis":  request.json ['Sinopsis'], 
-        "Imagen":  request.json ['Imagen']
+        "Imagen":  request.json ['Imagen'],
+        "ID" : id + 1
     }
 
     movies.append(new_movie)
+    id += 1
     print(request.json)
     return jsonify({"message":"Pelicula agregada con exito", "movies": movies})
+
+#Modificar una pelicula
 
 #Borrar una pelicula - #Delete a movie
 @app.route('/borrar_pelicula', methods=['DELETE'])
 def delete_movie():
-    
--- movies.py
+++ movies.py
@@ -1,5 +1,14 @@
 #Lista de peliculas con reviews
 movies = [
-    {"Titulo":"", "Anio":"", "Director":"", "Genero": "", "Sinopsis": "", "Imagen": ""},
-    
+    {"Titulo":"a", "Anio":"11", "Director":"aa", "Genero": "", "Sinopsis": "", "Imagen": "", "ID": 1},
+    {"Titulo":"b", "Anio":"12", "Director":"aa", "Genero": "", "Sinopsis": "", "Imagen": "", "ID": 2},
+    {"Titulo":"c", "Anio":"13", "Director":"aa", "Genero": "", "Sinopsis": "", "Imagen": "", "ID": 3},
+    {"Titulo":"d", "Anio":"14", "Director":"aa", "Genero": "", "Sinopsis": "", "Imagen": "", "ID": 4},
+    {"Titulo":"e", "Anio":"15", "Director":"aa", "Genero": "", "Sinopsis": "", "Imagen": "", "ID": 5},
+    {"Titulo":"f", "Anio":"16", "Director":"bb", "Genero": "", "Sinopsis": "", "Imagen": "", "ID": 6},
+    {"Titulo":"g", "Anio":"17", "Director":"bb", "Genero": "", "Sinopsis": "", "Imagen": "", "ID": 7},
+    {"Titulo":"h", "Anio":"18", "Director":"bb", "Genero": "", "Sinopsis": "", "Imagen": "", "ID": 8},
+    {"Titulo":"i", "Anio":"19", "Director":"bb", "Genero": "", "Sinopsis": "", "Imagen": "", "ID": 9},
+    {"Titulo":"j", "Anio":"20", "Director":"bb", "Genero": "", "Sinopsis": "", "Imagen": "", "ID": 10},
+
 ]
