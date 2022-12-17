a28b26bbc7c94d8f996a213ff2c8c643a4857a05
Stefano D'Annunzio <89990955+stefasito@users.noreply.github.com>
2022-12-16 22:53:42 -0300

    Add and Modify functionalities fully working now!

--- app.py
+++ app.py
@@ -9,19 +9,6 @@ auth = HTTPBasicAuth()
 # Importación de Datos - Import Data
 
 # Cargar el archivo movies.json
-with open('data/movies.json', 'r') as f:
-    movies = json.load(f)
-
-# Cargar el archivo directors.json
-with open('data/directors.json', 'r') as f:
-    directors = json.load(f)
-
-# Cargar el archivo genres.json
-with open('data/genres.json', 'r') as f:
-    genres = json.load(f)
-
-#Numero de ID para peliculas
-#id = 0
 
 @auth.verify_password
 def verify_password(username, password):
@@ -33,30 +20,102 @@ def verify_password(username, password):
     return False
 
 # Ruta para el módulo publico
-@app.route('/public', methods=['POST'])
+@app.route('/public', methods=['GET'])
 def get_public_movies():
+    # Cargar el archivo movies.json
+    with open('data/movies.json', 'r') as f:
+        movies = json.load(f)
     # Obtener las últimas 10 películas en la lista
     public_movies = movies[-10:]
     # Devolver las películas en JSON
     return jsonify(public_movies)
 
 
-# Ruta para el módulo privado
-@app.route('/private', methods=['POST'])
-@auth.login_required
-# def add_movie():
-#     # Get the movie data from the request
-#     data = request.get_json()
+# Ruta para el módulo privado para agregar películas
+@app.route('/private/add', methods=['POST'])
+def add_movie():
+    # Obtener la información de la película desde la request
+    movie_data = request.get_json()
 
-#     # Chequear que los campos requeridos estan presentes
-#     if 'title' not in data or 'year' not in data or 'director' not in data or 'genre' not in data:
-#         return jsonify({'message': 'faltan campos requeridos'}), 400
+    # Checkear que todos los campos requeridos estan presentes
+    if 'title' not in movie_data or 'year' not in movie_data or 'director' not in movie_data or 'genre' not in movie_data or 'synopsis' not in movie_data or 'img_url' not in movie_data:
+        return jsonify({'error': 'Falta/n campo/s requerido/s'}), 400
 
-#     # Check that the director and genre are valid
-#     if data['director'] not in directors or data['genre'] not in genres:
-#         return jsonify({'message': 'director o género inválido/s'}), 401
-def private_module():
-    return jsonify({"message":"El modulo privado funciona"}), 200
+    # Obtener las listas de directores y géneros
+    with open('data/directors.json', 'r') as f:
+        directors = json.load(f)
+    with open('data/genres.json', 'r') as f:
+        genres = json.load(f)
+
+    # Checkear si el director y el género son válidos
+    if movie_data['director'] not in directors:
+        return jsonify({'error': 'Director inválido'}), 400
+    if movie_data['genre'] not in genres:
+        return jsonify({'error': 'Género Inválido'}), 400
+
+    # Añadir la película a la lista
+    with open('data/movies.json', 'r') as f:
+        movies = json.load(f)
+    movie_added = False
+    # Si la película ya se encuentra en la lista, simplemente se agrega la review a la lista de reviews
+    for i, movie in enumerate(movies):
+        if movie['title'] == movie_data['title'] and movie['year'] == movie_data['year']:
+            movies[i]['reviews'].append(movie_data['review'])
+            movie_added = True
+            break
+    # Si la película no se encuentra, se añade correctamente y se agrega la review
+    # como primer elemento de la lista de reviews
+    if not movie_added:
+        movie_data['reviews'] = [movie_data['review']]
+        movies.append(movie_data)
+    with open('data/movies.json', 'w') as f:
+        json.dump(movies, f)
+
+    return jsonify({'message': 'Película agregada correctamente'}), 201
+
+
+
+# Modificar una pelicula
+@app.route('/private/modify/<string:title>', methods=['PUT'])
+def update_movie(title):
+    # Obtener los datos de la request
+    movie_data = request.get_json()
+
+    # Checkear que todos los datos estan presentes
+    if 'year' not in movie_data or 'director' not in movie_data or 'genre' not in movie_data or 'synopsis' not in movie_data or 'img_url' not in movie_data:
+        return jsonify({'error': 'Falta/n campo/s requerido/s'}), 400
+
+    # Obtener las listas de directores y géneros
+    with open('data/directors.json', 'r') as f:
+        directors = json.load(f)
+    with open('data/genres.json', 'r') as f:
+        genres = json.load(f)
+
+    # Checkear que el director y el género son válidos
+    if movie_data['director'] not in directors:
+        return jsonify({'error': 'Director inválido'}), 400
+    if movie_data['genre'] not in genres:
+        return jsonify({'error': 'Género inválido'}), 400
+
+    # Actualizar la película en la lista
+    with open('data/movies.json', 'r') as f:
+        movies = json.load(f)
+    for i, movie in enumerate(movies):
+        if movie['title'] == title:
+            movies[i]['title'] = movie_data['title']
+            movies[i]['year'] = movie_data['year']
+            movies[i]['director'] = movie_data['director']
+            movies[i]['genre'] = movie_data['genre']
+            movies[i]['synopsis'] = movie_data['synopsis']
+            movies[i]['img_url'] = movie_data['img_url']
+            break
+    else:
+        return jsonify({'error': 'Película no encontrada'}), 404
+    with open('data/movies.json', 'w') as f:
+        json.dump(movies, f)
+
+    return jsonify({'message': 'Pelicula modificada satisfactoriamente'}), 200
+
 
 
 # Agregar una reseña - Add a review
@@ -65,28 +124,7 @@ def add_review():
     
     return 0
 
-# ABM de cada pelicula
-# Agregar una pelicula - Add a movie
-# @app.route('/agregar_pelicula', methods=["POST"])
-# def add_movie():
-#     #id += 1
 
-#     new_movie = {
-#         "Titulo": request.json['Titulo'], 
-#         "Anio": request.json ['Anio'], 
-#         "Director": request.json ['Director'], #En vez de pedirlo tendriamos que obtenerlo de la lista
-#         "Genero":  request.json ['Genero'], #En vez de pedirlo tendriamos que obtenerlo de la lista
-#         "Sinopsis":  request.json ['Sinopsis'], 
-#         "Imagen":  request.json ['Imagen'],
-#         "ID" : id + 1
-#     }
-
-#     movies.append(new_movie)
-#     id += 1
-#     print(request.json)
-#     return jsonify({"message":"Pelicula agregada con exito", "movies": movies})
-
-# Modificar una pelicula
 
 # Borrar una pelicula - #Delete a movie
 #@app.route('/borrar_pelicula', methods=['DELETE'])
